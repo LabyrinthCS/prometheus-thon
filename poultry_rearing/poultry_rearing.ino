eeb1d0753a51bf89b3d6190035ea97e8b2e0d235
Mohammed Sameeruddin <loopholesameer@gmail.com>
2018-02-27 12:38:13 +0530

    updated the code

--- poultry_rearing/poultry_rearing.ino
+++ poultry_rearing/poultry_rearing.ino
@@ -12,7 +12,7 @@ void loop() {
   // put your main code here, to run repeatedly:
   delay(1000);
   float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature();
+  float temperature = dht.getTemperature(); // temperature in celsius
   float tempf = temperature * 1.8 + 32;
   /*
   Serial.print(dht.getStatusString());
@@ -24,3 +24,28 @@ void loop() {
   Serial.println(tempf,1);
   */
 }
+
+void voiceyBT() {
+  while (Serial.available()) {
+    delay(10);
+    char chr = Serial.read();
+    if (chr == '#') {
+      break;
+    }
+    voice += chr;
+  }
+  int len = voice.length();
+  if (len > 0) {
+    if (voice == "*temprature") {
+      if (temperature <= 34 && temperature >= 20) { // recommended temperatures for broilers
+       digitalWrite(led, HIGH); 
+      } else {
+        digitalWrite(led, LOW);
+      }
+    } else if (voice == "*okay" || voice == "*ok") {
+      digitalWrite(led, LOW);
+    }
+  }
+  voice = "";
+}
+
