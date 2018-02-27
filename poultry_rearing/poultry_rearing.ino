d749c203d4ed692bc2a0a130bb8d6ece0f6fd753
Mohammed Sameeruddin <loopholesameer@gmail.com>
2018-02-27 13:41:32 +0530

    updated bluetooth code

--- poultry_rearing/poultry_rearing.ino
+++ poultry_rearing/poultry_rearing.ino
@@ -1,9 +1,18 @@
 #include <DHT.h>
 DHT dht;
 
+#include<SoftwareSerial.h>
+SoftwareSerial mySerial(0,1);
+
+String readString;
+float temperature;
+byte led = 7;
+
 void setup() {
   // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
   Serial.begin(9600);
+  mySerial.begin(9600);
   Serial.println("Status\tHumidity (%)\tTemperature (C)\t(F)");
   dht.setup(8);
 }
@@ -12,20 +21,35 @@ void loop() {
   // put your main code here, to run repeatedly:
   delay(1000);
   float humidity = dht.getHumidity();
-  float temperature = dht.getTemperature(); // temperature in celsius
+  temperature = dht.getTemperature(); // temperature in celsius
   float tempf = temperature * 1.8 + 32;
   /*
-  Serial.print(dht.getStatusString());
-  Serial.print("\t");
-  Serial.print(humidity,1);
-  Serial.print("\t\t");
-  Serial.print(temperature,1);
-  Serial.print("\t");
-  Serial.println(tempf,1);
+    Serial.print(dht.getStatusString());
+    Serial.print("\t");
+    Serial.print(humidity,1);
+    Serial.print("\t\t");
+    Serial.print(temperature,1);
+    Serial.print("\t");
+    Serial.println(tempf,1);
   */
+  while (mySerial.available() > 0)
+    readString = mySerial.readString();
+  if (readString == "temperature") {
+    if (temperature <= 34 && temperature >= 20) { // recommended temperatures for broilers
+        Serial.println(temperature);
+        digitalWrite(led, HIGH);
+      } else {
+        digitalWrite(led, LOW);
+      }
+    Serial.println("Led ON");
+  } if (readString == "okay" || readString == "ok") {
+    digitalWrite(led, LOW);
+    Serial.println("Led OFF");
+  }
 }
 
-void voiceyBT() {
+/*
+  void voiceyBT() {
   while (Serial.available()) {
     delay(10);
     char chr = Serial.read();
@@ -38,7 +62,7 @@ void voiceyBT() {
   if (len > 0) {
     if (voice == "*temprature") {
       if (temperature <= 34 && temperature >= 20) { // recommended temperatures for broilers
-       digitalWrite(led, HIGH); 
+        digitalWrite(led, HIGH);
       } else {
         digitalWrite(led, LOW);
       }
@@ -47,5 +71,5 @@ void voiceyBT() {
     }
   }
   voice = "";
-}
-
+  }
+*/
